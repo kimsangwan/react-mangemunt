190b476f4b62e7028f35c723423016c36ff637f2
kimsangwan <39785640+kimsangwan@users.noreply.github.com>
2020-06-26 18:26:35 +0900

    516

--- src/App.js
+++ src/App.js
@@ -3,27 +3,48 @@ import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
-  id: 1,
-  image: 'https://placeimg.com/64/64/any',
-  name: '상완',
-  brithday: '921120',
-  gender: '남자',
-  job: '대학생',
-};
+const customer = [
+  {
+    id: 1,
+    image: 'https://placeimg.com/64/64/any',
+    name: '상완',
+    brithday: '921120',
+    gender: '남자',
+    job: '대학생',
+  },
+  {
+    id: 2,
+    image: 'https://placeimg.com/64/64/1',
+    name: '르르',
+    brithday: '921120',
+    gender: '남자',
+    job: '대학생',
+  },
+  {
+    id: 3,
+    image: 'https://placeimg.com/64/64/115',
+    name: 'ㅇㄴㅇㄴ',
+    brithday: '921120',
+    gender: '남자',
+    job: '대학생',
+  },
+];
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Customer
-          id={customer.id}
-          image={customer.image}
-          name={customer.name}
-          brithday={customer.brithday}
-          gender={customer.gender}
-          job={customer.job}
-        />
+        {customer.map((a) => {
+          return (
+            <Customer
+              id={a.id}
+              image={a.image}
+              name={a.name}
+              brithday={a.brithday}
+              gender={a.gender}
+            />
+          );
+        })}
       </div>
     );
   }
